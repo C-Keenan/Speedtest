b272e5e467069b03feb2d23f8dfafaea836f0d35
C-Keenan <108506379+C-Keenan@users.noreply.github.com>
2025-09-12 20:02:20 -0400

    Update entrypoint.sh
    Updated entrypoint.sh with better variable checking for the server-id and changed wording to match terminology within the links provided by Ookla. Added timestamps to results and header row of csv for better parsing and logging.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,40 +1,43 @@
 #!/usr/bin/env bash
 LOG_FILE="/app/log/ookla_speedtest_log.csv"
-HEADERS="server name,server id,idle latency,idle jitter,packet loss,download,upload,download bytes,upload bytes,share url,download server count,download latency,download latency jitter,download latency low,download latency high,upload latency,upload latency jitter,upload latency low,upload latency high,idle latency low,idle latency high"
+HEADERS="server name,server id,idle latency,idle jitter,packet loss,download,upload,download bytes,upload bytes,share url,download server count,download latency,download latency jitter,download latency low,download latency high,upload latency,upload latency jitter,upload latency low,upload latency high,idle latency low,idle latency high,timestamp"
+
 if [ ! -f "$LOG_FILE" ]; then
   echo "$HEADERS" > "$LOG_FILE"
   chmod 664 "$LOG_FILE"
   echo "Log file created with header."
 fi
-if [ "$ACCEPT_EULA" == "true" ] && [ "$ACCEPT_GDPR" == "true" ] && [ "$ACCEPT_TERMS" == "true" ]; then
+
+echo "Writing to log file at: $LOG_FILE"
+
+if [ "$ACCEPT_EULA" == "true" ] && [ "$ACCEPT_PRIVACY" == "true" ] && [ "$ACCEPT_TERMS" == "true" ]; then
   echo "All required agreements accepted. Starting speedtest script."
+
   while true; do
-    ping -c 4 1.1.1.1
+    ping -c 4 1.1.1.1 > /dev/null 2>&1
     status=$?
     stime=60
     if [ "$status" -gt 0 ]; then
       echo "No internet connection detected. Sleeping for $stime seconds."
-      sleep  $stime
+      sleep "$stime"
     elif [ "$status" -eq 0 ]; then
       echo "Internet connection detected. Proceeding with speedtest."
       break
-  fi
+    fi
   done
-  if [ -z "$SERVER_ID" ]; then
-    echo "No SERVER_ID provided. Running speedtest with default server selection."
-  elif [[ "$SERVER_ID" =~ ^[0-9]+$ ]]; then
-    echo "Using SERVER_ID: $SERVER_ID"
-  else
-    echo "Error: SERVER_ID must be a number. Current value: '$SERVER_ID'"
-    exit
+
+  if [ -n "$SERVER_ID" ] && ! [[ "$SERVER_ID" =~ ^[0-9]+$ ]]; then
+    echo "Error: SERVER_ID must be a number if set. Current value: '$SERVER_ID'"
+    exit 0
   fi
+
   ./speedtest.sh
 else
-  echo "You must accept the EULA, GDPR, and Terms to proceed. The links to those respective documents are below."
+  echo "You must accept the EULA, Privacy, and Terms to proceed. The links to those respective documents are below."
   echo "EULA: https://www.speedtest.net/about/eula"
-  echo "GDPR: https://www.speedtest.net/about/privacy"
+  echo "Privacy: https://www.speedtest.net/about/privacy"
   echo "Terms: https://www.speedtest.net/about/terms"
-  echo "Set the following environment variables to true to accept: ACCEPT_EULA, ACCEPT_GDPR, ACCEPT_TERMS"
+  echo "Set the following environment variables to true to accept: ACCEPT_EULA, ACCEPT_PRIVACY, ACCEPT_TERMS"
   echo "Stopping container and exiting now."
   exit 0
 fi
