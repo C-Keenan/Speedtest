58e9c437437105f90013ca3541c43892c862e82b
C-Keenan <108506379+C-Keenan@users.noreply.github.com>
2025-09-12 20:14:26 -0400

    Update speedtest.sh
    Added better logging of failures or discrepancies in the internet connection so the frontend can have an accurate view of the times there are actual failures instead of just throwing the data out and showing a better average result than is actually true. Moved last unnecessary log statement to entrypoint.sh as that is where the messaging should be done unless absolutely necessary.

--- backend/speedtest.sh
+++ backend/speedtest.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/env bash
 LOG_FILE="/app/log/ookla_speedtest_log.csv"
-echo "Writing to log file at: $LOG_FILE"
+
 while true; do
   current_minute=$(date +"%M")
   current_second=$(date +"%S")
-  
+
   total_seconds_in_hour=$(( (10#$current_minute * 60) + 10#$current_second ))
   
   sleep_seconds=$(( 3600 - total_seconds_in_hour ))
   
   echo "Current time: $(date). Sleeping for $sleep_seconds seconds to sync with top of hour."
   sleep "$sleep_seconds"
+
+  SPEEDTEST_OUTPUT=""
   if [ -z "$SERVER_ID" ]; then
-    speedtest  --accept-license --accept-gdpr --format=csv | tail -n 1 >> "$LOG_FILE"
+    SPEEDTEST_OUTPUT=$(speedtest --accept-license --accept-gdpr --format=csv)
   else
-    speedtest  --accept-license --accept-gdpr -s "$SERVER_ID" --format=csv | tail -n 1 >> "$LOG_FILE"
+    SPEEDTEST_OUTPUT=$(speedtest --accept-license --accept-gdpr -s "$SERVER_ID" --format=csv)
+  fi
+
+  STATUS=$?
+  CURRENT_TIME=$(date -u +"%Y-%m-%dT%H:%M:%S.%NZ")
+
+  if [ $STATUS -eq 0 ] && [ -n "$SPEEDTEST_OUTPUT" ]; then
+    TEMP_OUTPUT=$(echo "$SPEEDTEST_OUTPUT" | tail -n 1)
+    echo "$TEMP_OUTPUT,$CURRENT_TIME" >> "$LOG_FILE"
+  else
+    ZERO_VALUES="Failed Server,,0,0,0,0,0,0,0,,0,0,0,0,0,0,0,0,0,0,0"
+    echo "$ZERO_VALUES,$CURRENT_TIME" >> "$LOG_FILE"
+    echo "Speedtest failed. Logged zero values."
   fi
 done
