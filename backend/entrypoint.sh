9e79eea0d0b821e6cf484931202d20dfbc507376
C-Keenan <108506379+C-Keenan@users.noreply.github.com>
2025-09-14 21:41:58 -0400

    Update entrypoint.sh
    Updated entrypoint.sh to remove unnecessary newlines to help save on storage space as unless you're a developer these files will likely never be seen.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
 LOG_FILE="/app/log/ookla_speedtest_log.csv"
 HEADERS="server name,server id,idle latency,idle jitter,packet loss,download,upload,download bytes,upload bytes,share url,download server count,download latency,download latency jitter,download latency low,download latency high,upload latency,upload latency jitter,upload latency low,upload latency high,idle latency low,idle latency high,timestamp"
-
 SPDTSTLOC="/usr/local/bin/speedtest"
-
 if [ ! -x "$SPDTSTLOC" ]; then
   echo "Error: Speedtest binary not found or not executable at $SPDTSTLOC. Please ensure the binary is present and has execute permissions. If you're running this as a docker container, ensure the volume is mounted correctly."
   exit 0
 fi
-
 if [ ! -f "$LOG_FILE" ]; then
   echo "$HEADERS" > "$LOG_FILE"
   chmod 664 "$LOG_FILE"
   echo "Log file created with header."
 fi
-
 echo "Writing to log file at: $LOG_FILE"
-
 if [ "$ACCEPT_EULA" == "true" ] && [ "$ACCEPT_PRIVACY" == "true" ] && [ "$ACCEPT_TERMS" == "true" ]; then
   echo "All required agreements accepted. Starting speedtest script."
-
   while true; do
     ping -c 4 1.1.1.1 > /dev/null 2>&1
     status=$?
@@ -32,12 +26,10 @@ if [ "$ACCEPT_EULA" == "true" ] && [ "$ACCEPT_PRIVACY" == "true" ] && [ "$ACCEPT
       break
     fi
   done
-
   if [ -n "$SERVER_ID" ] && ! [[ "$SERVER_ID" =~ ^[0-9]+$ ]]; then
     echo "Error: SERVER_ID must be a number if set. Current value: '$SERVER_ID'"
     exit 0
   fi
-
   ./speedtest.sh
 else
   echo "You must accept the EULA, Privacy, and Terms to proceed. The links to those respective documents are below."
